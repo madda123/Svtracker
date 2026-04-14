73bb2aab7683b0156bde6618e7e46684b823a69f
John Doe <madda.athiarahman@gmail.com>
2026-04-14 12:11:57 +0700

    add edit button on profile picture

--- frontend/src/components/ui/main/Profile.tsx
+++ frontend/src/components/ui/main/Profile.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import type { UserSchema } from "../../../schemas/userSchema";
+import { Pen } from "lucide-react";
 
 type Props = {
   user: UserSchema | null;
@@ -11,11 +12,16 @@ const Profile = ({ user, className, variant }: Props) => {
   if (variant === "vertical") {
     return (
       <div className={`flex flex-col items-center gap-4 ${className}`}>
-        <img
-          src={user?.profileImage.url}
-          alt="profile-image"
-          className="w-20 h-20 rounded-full object-cover"
-        />
+        <div className="relative w-20 h-20">
+          <img
+            src={user?.profileImage.url}
+            alt="profile-image"
+            className="w-full h-full rounded-full object-cover"
+          />
+          <button className="absolute right-1 bottom-0 bg-cusorange rounded-full p-1 flex justify-center items-center shadow-lg z-10 cursor-pointer">
+            <Pen size={10} strokeWidth={3} className="text-cuswhite" />
+          </button>
+        </div>
         <div className="flex flex-col gap-1">
           <p className="text-cusblack text-bd-m md:text-bd font-semibold text-center leading-none">
             {user?.fullname}
@@ -30,11 +36,16 @@ const Profile = ({ user, className, variant }: Props) => {
 
   return (
     <div className={`flex items-center gap-2.5 ${className}`}>
-      <img
-        src={user?.profileImage.url}
-        alt="profile-image"
-        className="w-10 h-10 rounded-4xl object-cover"
-      />
+      <div className="relative w-10 h-10">
+        <img
+          src={user?.profileImage.url}
+          alt="profile-image"
+          className="w-full h-full rounded-full object-cover"
+        />
+        <button className="absolute right-0 bottom-0 bg-cusorange rounded-full p-1 flex justify-center items-center shadow-lg z-10 cursor-pointer">
+          <Pen size={8} strokeWidth={3} className="text-cuswhite" />
+        </button>
+      </div>
       <div className="flex flex-col gap-1">
         <p className="text-cusblack text-bd font-semibold leading-none">
           {user?.fullname}
